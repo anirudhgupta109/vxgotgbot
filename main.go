80f6dfc920fb8e6e614d37fe348c01007e947214
Anirudh Gupta <anirudhgupta109@aosip.dev>
2022-07-05 09:14:20 +0000

    main: Add info on bot and how to use
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>

--- main.go
+++ main.go
@@ -26,11 +26,28 @@ func main() {
 			continue
 		}
 
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
 		isUrl := tgbotapi.MessageEntity.IsURL(tgbotapi.MessageEntity{Type: "url"})
-		if !isUrl {
-			// ignore any message not a URL
+		isCommand := update.Message.IsCommand()
+
+		if !isUrl && !isCommand {
+			// ignore any message not a URL or command
 			continue
 		}
+
+		switch update.Message.Command() {
+		case "start":
+			msg.Text = "The bot is intended to replace twitter and instagram links with embedeable versions\nBuilt by @anirudh109"
+			continue
+		case "source":
+			msg.Text = "The source code is on https://github.com/anirudhgupta109/vxgotgbot"
+			continue
+		case "help":
+			msg.Text = "Just add to your groups and it'll automatically send a vxtwitter for twitter and ddinstagram for instagram link\nKnown commands are /source and /start"
+			continue
+		}
+
 		if !strings.Contains(update.Message.Text, "twitter.com") && !strings.Contains(update.Message.Text, "instagram.com") {
 			// ignore any message not containing twitter or instagram link
 			continue
@@ -39,8 +56,6 @@ func main() {
 		var newURL string
 		newURL = strings.Replace(update.Message.Text, "twitter.com", "vxtwitter.com", 1)
 		newURL = strings.Replace(newURL, "instagram.com", "ddinstagram.com", 1)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 
 		msg.Text = newURL
 
