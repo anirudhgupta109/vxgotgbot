ed872f357a451820a1a49efcacf196513952bde8
Anirudh Gupta <anirudhgupta109@aosip.dev>
2024-08-29 15:37:24 +0530

    helpers: Strip URLs of trackers/query params
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>

--- helpers/sendmessage.go
+++ helpers/sendmessage.go
@@ -2,6 +2,8 @@ package helpers
 
 import (
 	"log"
+	"net/url"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -16,4 +18,49 @@ func SendMessage(text string, update tgbotapi.Update, bot *tgbotapi.BotAPI) {
 	if _, err := bot.Send(msg); err != nil {
 		log.Panic(err)
 	}
+}
+
+func StripURLs(message *tgbotapi.Message) string {
+	if message.Text == "" {
+		return ""
+	}
+
+	urls := getURLsFromMessage(message)
+	finalText := message.Text
+
+	for _, u := range urls {
+		if u.RawQuery != "" {
+			scrubbedURL := strings.Replace(u.String(), "?"+u.RawQuery, "", 1)
+			finalText = strings.Replace(finalText, u.String(), scrubbedURL, 1)
+		}
+	}
+
+	return finalText
+}
+
+func getURLsFromMessage(message *tgbotapi.Message) []*url.URL {
+	if len(message.Entities) == 0 || message.Text == "" {
+		return nil
+	}
+
+	var urlEntities []tgbotapi.MessageEntity
+	for _, entity := range message.Entities {
+		if entity.Type == "url" {
+			urlEntities = append(urlEntities, entity)
+		}
+	}
+
+	if len(urlEntities) == 0 {
+		return nil
+	}
+
+	var urls []*url.URL
+	for _, entity := range urlEntities {
+		urlStr := message.Text[entity.Offset : entity.Offset+entity.Length]
+		if u, err := url.Parse(urlStr); err == nil {
+			urls = append(urls, u)
+		}
+	}
+
+	return urls
 }
